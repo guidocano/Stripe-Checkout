ff466d77d968bebb952c020cbe15cf7b52144e2c
Guido <guidocalciano@gmail.com>
2026-07-05 15:54:48 -0300

    first commit

---
+++ assets/stripe.checkout.js
@@ -0,0 +1,20 @@
+import STRIPE_KEYS from "./stripe-keys.js"
+
+const d = document,
+$ramen =d.getElementById("ramen"),
+$template = d.getElementById("ramen-template").content,
+$fragment = d.createDocumentFragment()
+
+fetch("https://api.stripe.com/v1/products", {
+    headers: {
+        Authorization: `Bearer ${STRIPE_KEYS.secret}`
+    }
+})
+.then((res) => 
+    {
+        console.log(res)
+        return res.json()
+    })
+.then(json => {
+    console.log(json)
+})
