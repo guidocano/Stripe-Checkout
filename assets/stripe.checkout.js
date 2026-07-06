6ea558d178c689eb83c909a20c3c58f35f3acf8f
Guido <guidocalciano@gmail.com>
2026-07-06 19:54:09 -0300

    update stripe checkout to display product images and prices

--- assets/stripe.checkout.js
+++ assets/stripe.checkout.js
@@ -18,10 +18,28 @@ Promise.all([
 ]).then(responses => Promise.all(responses.map(res => res.json())))
 .then(json => {
     console.log(json)
+    products = json[0].data;
+    prices = json[1].data;
+    console.log(products, prices);
+
+    prices.forEach(el => {
+        let productData = products.filter(product => product.id === el.product);
+        console.log(productData);
+
+        $template.querySelector(".ramen").setAttribute("data-price", el.id);
+        $template.querySelector("img").src = productData[0].images[0];
+        $template.querySelector("img").alt = productData[0].name;
+        $template.querySelector("figcaption").innerHTML = `${productData[0].name} <br> ${el.currency} ${el.unit_amount_decimal} <br>`;
+
+        let $clone = d.importNode($template, true);
+        $fragment.appendChild($clone);
+});
+
+$ramen.appendChild($fragment);
+
 })
 .catch(err => {
     console.log(err);
     let message = err.statusText || "Ocurrió un error al conectarse con la API de Stripe";
     $ramen.innerHTML = `<p>Error ${err.status}: ${message}</p>`;
-}
-)
+})
