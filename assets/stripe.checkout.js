aa2e57b25b027f3465efbdeca648e6529fbc370a
Guido <guidocalciano@gmail.com>
2026-07-06 18:41:06 -0300

    add template to html

--- assets/stripe.checkout.js
+++ assets/stripe.checkout.js
@@ -5,7 +5,25 @@ $ramen =d.getElementById("ramen"),
 $template = d.getElementById("ramen-template").content,
 $fragment = d.createDocumentFragment()
 
+
+
 fetch("https://api.stripe.com/v1/products", {
+    headers: {
+        Authorization: `Bearer ${STRIPE_KEYS.secret}`
+    }
+})
+.then((res) => 
+    {
+        console.log(res)
+        return res.json()
+    })
+.then(json => {
+    console.log(json)
+}); 
+
+
+
+fetch("https://api.stripe.com/v1/prices", {
     headers: {
         Authorization: `Bearer ${STRIPE_KEYS.secret}`
     }
