0124cbc31241c29c01fc83806044e9985755edb1
Guido <guidocalciano@gmail.com>
2026-07-09 15:13:14 -0300

    feat: implement money formatting and add Stripe checkout redirection on product click

--- assets/stripe.checkout.js
+++ assets/stripe.checkout.js
@@ -12,6 +12,10 @@ fetchOptions = {
 
 let prices, products;
 
+const moneyFormat = (num) => {
+    return `$${num.slice(0, -2) || 0}.${num.slice(-2)}`;
+}
+
 Promise.all([
     fetch("https://api.stripe.com/v1/products", fetchOptions),
     fetch("https://api.stripe.com/v1/prices", fetchOptions)
@@ -32,7 +36,7 @@ Promise.all([
         $template.querySelector(".ramen-figure").setAttribute("data-price", el.id);
         $template.querySelector("img").src = productData[0].images[0];
         $template.querySelector("img").alt = productData[0].name;
-        $template.querySelector("figcaption").innerHTML = `${productData[0].name} <br> ${el.currency} ${el.unit_amount_decimal} <br>`;
+        $template.querySelector("figcaption").innerHTML = `${productData[0].name} <br> ${el.currency} ${moneyFormat(el.unit_amount_decimal)} <br>`;
 
         let $clone = d.importNode($template, true);
         $fragment.appendChild($clone);
@@ -46,3 +50,18 @@ Promise.all([
     let message = err.statusText || "Ocurrió un error al conectarse con la API de Stripe";
     $ramen.innerHTML = `<p>Error ${err.status}: ${message}</p>`;
 })
+
+
+d.addEventListener("click", e => {
+    if(e.target.matches(".ramen-figure *")) {
+        let priceId = e.target.parentElement.getAttribute("data-price");
+        Stripe(STRIPE_KEYS.public)
+        .redirectToCheckout({
+            lineItems: [{ price: priceId, quantity: 1 }],
+        })
+        .then(res => {
+            if(res.error) {
+                $ramen.insertAdjacentHTML("afterend", res.error.message);
+            }   
+        }) 
+    }})
